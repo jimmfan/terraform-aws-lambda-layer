d6bed10ef75f2f7185dfc0d457ef3244ffb2db1e
jimmfan <jimmfan@users.noreply.github.com>
2023-11-05 04:52:39 +0000

    Updated key to use var.lambda_layer_name

--- main.tf
+++ main.tf
@@ -4,13 +4,12 @@ resource "aws_s3_bucket" "lambda_layer_bucket" {
 
 resource "aws_s3_object" "lambda_layer_object" {
   bucket = aws_s3_bucket.lambda_layer_bucket.id
-  key    = "lambda-layer.zip"
+  key    = "${var.lambda_layer_name}.zip"
   source = var.lambda_layer_zip_path
   etag   = filemd5(var.lambda_layer_zip_path)
 }
 
 resource "aws_lambda_layer_version" "lambda_layer" {
-  # filename   = var.lambda_layer_zip_path
   layer_name = var.lambda_layer_name
   s3_bucket  = aws_s3_bucket.lambda_layer_bucket.id
   s3_key     = aws_s3_object.lambda_layer_object.key
